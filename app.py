1f8c092f70846284087140abba01198c2af53792
CH TEJA YADAV <tejayadavch@gmail.com>
2025-03-18 11:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,6 +87,8 @@ def main():
         
         input_scaled = scaler.transform(input_data)
         prediction = model.predict(input_scaled)[0]
+
+    
          # Display prediction result
     st.markdown(
     f'<p style="color:white; background-color:#28a745; padding:10px; border-radius:5px; font-size:16px; font-weight:bold;">🏥 Predicted Crop Yield: {prediction:.2f} hg/ha</p>', 
